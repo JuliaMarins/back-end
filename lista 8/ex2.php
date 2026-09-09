a5a9d391dd86140b9041b9d40d80512c8a9821f5
Julia Marins <julia.assuncao@edu.senai.br>
2026-09-09 09:22:33 -0300

    Add files via upload

---
+++ lista 8/ex2.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "Números pares: <br>";
+for($numero = 1; $numero <=30; $numero=$numero+1){
+    if($numero%2 == 0){
+        echo"$numero <br>";
+    }
+}
+
+?>
---
+++ lista 8/ex4.php
@@ -0,0 +1,13 @@
+<?php
+
+//coloque o numero da tabuada q quiser
+$num = 9;
+for($i = 1; $i <=10; $i++){
+    $resultado  = $num * $i;
+
+    echo"num x $i = $resultado <br>";
+}
+
+
+
+?>
